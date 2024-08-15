56eb6d5d33412b2400875f9e863d795fcd3dfc8a
Diego Peralta M <diego.pmz08@gmail.com>
2024-08-14 23:20:31 -0600

    Corrigiendo un error de dimensiones

--- src/components/ui/AppButton.tsx
+++ src/components/ui/AppButton.tsx
@@ -1,30 +1,33 @@
+import { twclass } from "@/utilities/twclass";
+
 interface AppButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
   variant?: "primary" | "secondary";
 }
 
 export const PRIMARY_CLASS =
-  "w-full flex items-center justify-center h-[100%] rounded-lg bg-appPurple py-[8px] px-[24px] text-white transition-colors duration-200 ease-out active:bg-appPurpleH disabled:pointer-events-none disabled:bg-appPurpleH";
+  "flex h-full w-full items-center justify-center rounded-lg bg-appPurple py-[8px] text-white transition-colors duration-200 ease-out active:bg-appPurpleH disabled:pointer-events-none disabled:bg-appPurpleH";
 export const SECONDARY_CLASS =
-  "w-full flex items-center justify-center h-[100%] rounded-lg py-[8px] px-[24px] font-semibold text-appPurple ring-1 ring-appPurple transition-colors duration-200 ease-out active:bg-appPurpleL disabled:pointer-events-none disabled:opacity-25";
+  "w-full flex items-center justify-center h-full rounded-lg py-[8px] px-[24px] font-semibold text-appPurple ring-1 ring-appPurple transition-colors duration-200 ease-out active:bg-appPurpleL disabled:pointer-events-none disabled:opacity-25";
 
 export const AppButton = ({
   children,
   variant = "primary",
   ...props
 }: AppButtonProps) => {
-  function variantValidation() {
-    if (variant !== "primary" && variant !== "secondary") return "primary";
-
-    return variant;
-  }
-
-  const buttonClass =
-    variantValidation() === "primary" ? PRIMARY_CLASS : SECONDARY_CLASS;
+  const variantValidation = variant !== "primary" && variant !== "secondary";
 
   return (
     <>
-      <button className={buttonClass} {...props}>
+      <button
+        {...props}
+        className={twclass(
+          "flex h-full w-full items-center justify-center rounded-lg bg-appPurple py-[8px] text-white transition-colors duration-200 ease-out active:bg-appPurpleH disabled:pointer-events-none disabled:bg-appPurpleH",
+          variant === "secondary" &&
+            "bg-transparent font-semibold text-appPurple ring-1 ring-appPurple active:bg-appPurpleL disabled:opacity-25",
+          variantValidation && PRIMARY_CLASS,
+        )}
+      >
         {children}
       </button>
     </>
