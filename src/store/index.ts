40c248ec8b7e29dad3424ba60d7d00b799bce1d8
Diego Peralta M <diego.pmz08@gmail.com>
2024-08-20 21:42:43 -0600

    Creando la store del estado

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,51 +1,152 @@
+import { AvailableSocialMedia } from "@/types/social-media";
 import { create } from "zustand";
 
-interface RegisterUser {
+interface AuthUserTypes {
   email: string;
   password: string;
   confirmPassword: string;
 }
 
-interface LoginUser {
-  email: string;
-  password: string;
+export interface ProfileLink {
+  id: string;
+  social_media: AvailableSocialMedia | "";
+  url: string;
 }
 
-// interface UserAppProfile {
-//   profileEmail: string;
-//   name: string;
-//   lastName: string;
-//   image: string;
-//   links: null;
-// }
+interface AppUserProfile {
+  id: string;
+  credentials: null | string;
+  Profile_email: string;
+  profile_name: string;
+  profile_last_name: string;
+  profile_image: string;
+  profile_links: ProfileLink[] | [];
+  profile_template: string;
+}
+
+interface AppErrorLink {
+  id: string;
+  message: string;
+}
+
+interface AppError {
+  Profile_email: {
+    status: boolean;
+    message: string;
+  };
+  profile_name: {
+    status: boolean;
+    message: string;
+  };
+  profile_last_name: {
+    status: boolean;
+    message: string;
+  };
+  profile_image: {
+    status: boolean;
+    message: string;
+  };
+
+  profile_links: {
+    status: boolean;
+    links: AppErrorLink[] | [];
+  };
+}
 
 interface useStoreAppState {
-  register: RegisterUser;
-  login: LoginUser;
-  registerUserMethod: (registerData: RegisterUser) => void;
+  auth: AuthUserTypes;
+  userProfile: AppUserProfile;
+  appError: AppError;
+  generateNewLink: () => void;
+  removeLink: (id: string) => void;
+  onChangeLinks: (
+    id: ProfileLink["id"],
+    value: Omit<ProfileLink, "id">,
+  ) => void;
 }
 
-export const useStoreApp = create<useStoreAppState>((set) => ({
-  login: {
-    email: "",
-    password: "",
-  },
-
-  register: {
+export const useStoreApp = create<useStoreAppState>()((set, get) => ({
+  auth: {
     email: "",
     password: "",
     confirmPassword: "",
   },
+  userProfile: {
+    id: "",
+    credentials: null,
+    Profile_email: "",
+    profile_image: "",
+    profile_last_name: "",
+    profile_links: [],
+    profile_name: "",
+    profile_template: "",
+  },
 
-  registerUserMethod: async ({
-    email,
-    password,
-    confirmPassword,
-  }: RegisterUser) =>
-    set((state) => {
-      const validateData = email && password && confirmPassword;
-      if (!validateData) console.log("❌❌❌");
+  appError: {
+    Profile_email: {
+      status: false,
+      message: "",
+    },
+    profile_image: {
+      status: false,
+      message: "",
+    },
+    profile_last_name: {
+      status: false,
+      message: "",
+    },
+    profile_name: {
+      status: false,
+      message: "",
+    },
+    profile_links: {
+      status: false,
+      links: [],
+    },
+  },
 
-      return { ...state };
-    }),
+  generateNewLink: () => {
+    const UUID = crypto.randomUUID();
+    const newLink: ProfileLink = {
+      id: UUID,
+      social_media: "",
+      url: "",
+    };
+
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        profile_links: [...state.userProfile.profile_links, newLink],
+      },
+    }));
+  },
+
+  removeLink: (id) => {
+    const allUserLinks = get().userProfile.profile_links;
+    const updatedLinks = allUserLinks.filter((link) => link.id !== id);
+
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        profile_links: updatedLinks,
+      },
+    }));
+  },
+
+  onChangeLinks: (id: ProfileLink["id"], value: Omit<ProfileLink, "id">) => {
+    const userLinks = get().userProfile.profile_links;
+    const currentLinkModified = userLinks.find((link) => link.id === id);
+
+    if (!currentLinkModified)
+      throw new Error("Invalid id. Must be one id from profile_links");
+
+    const updateLink = userLinks.map((link) =>
+      link.id === id
+        ? { ...link, social_media: value.social_media, url: value.url }
+        : link,
+    );
+    set((state) => ({
+      userProfile: { ...state.userProfile, profile_links: updateLink },
+    }));
+  },
 }));
