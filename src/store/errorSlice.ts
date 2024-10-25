7e4b04145db3613d6bb7d49cea51cd344d077581
Diego Peralta M <diego.pmz08@gmail.com>
2024-10-25 15:40:44 -0600

    Se creo el metodo validateLink de errorSlice

--- src/store/errorSlice.ts
+++ src/store/errorSlice.ts
@@ -1,4 +1,6 @@
+import { profileLinkSchema } from "@/schemas/app-profile-schemas";
 import { ProfileLinks } from "@/types/api-response";
+import schemaValidator from "@/utilities/schema-validator";
 import { StateCreator } from "zustand";
 
 type ErrorLinkStructure = Record<keyof Omit<ProfileLinks, "id">, string | null>;
@@ -57,7 +59,54 @@ export const errorSlice: ErrorSliceBuildType = (set, get) => ({
     profile_links: [],
 
     validateLink: (link) => {
-      console.log(link);
+      const errLinks = get().appErrors.profile_links;
+      const { isError, schemaError } = schemaValidator(profileLinkSchema, {
+        platform: link.platform,
+        url: link.url,
+      });
+
+      if (!isError) {
+        const removeStateError = errLinks.filter(
+          (err) => err.stateId !== link.id,
+        );
+
+        set((state) => ({
+          appErrors: { ...state.appErrors, profile_links: removeStateError },
+        }));
+
+        return;
+      }
+
+      const newError: ErrorProfileLinks = {
+        isErr: true,
+        message: {
+          platform: schemaError?.platform?._errors[0] ?? "",
+          url: schemaError?.url?._errors[0] ?? "",
+        },
+        stateId: link.id,
+      };
+
+      if (!errLinks.find((err) => err.stateId === link.id)) {
+        set((state) => ({
+          appErrors: {
+            ...state.appErrors,
+            profile_links: [...state.appErrors.profile_links, newError],
+          },
+        }));
+
+        return;
+      }
+
+      const updateErrors = errLinks.map((err) =>
+        err.stateId === link.id ? newError : err,
+      );
+
+      set((state) => ({
+        appErrors: {
+          ...state.appErrors,
+          profile_links: [...updateErrors],
+        },
+      }));
     },
     getErrorLink: (id) => {
       console.log(id);
