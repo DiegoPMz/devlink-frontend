033159c45ea1cf2de375eea3c36ac98bc9a0d22a
Diego Peralta M <diego.pmz08@gmail.com>
2024-11-09 02:42:12 -0600

    Se creo la funcionalidad y el menu de la app

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,11 +1,30 @@
 import { LogoDevlinksLarge } from "@/assets/LogoDevlinksLarge";
 import { LogoDevlinksSmall } from "@/assets/LogoDevlinksSmall";
+import { useEffect, useState } from "react";
 import { FaBars, FaRegCircleUser } from "react-icons/fa6";
 import { LuLink } from "react-icons/lu";
 import { AppLink } from "./ui/AppLink";
 import { AppLinkTab } from "./ui/AppLinkTab";
+import UserMenuModal from "./UserMenuModal";
 
 export const AppBar = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  useEffect(() => {
+    if (!isMenuActive) return;
+
+    const clickMenuOutside = (e: MouseEvent) => {
+      const currentElement = e.target as HTMLElement;
+      if (!currentElement.matches("#user-menu-modal")) setIsMenuActive(false);
+    };
+
+    document.body.addEventListener("click", clickMenuOutside);
+
+    return () => {
+      document.body.removeEventListener("click", clickMenuOutside);
+    };
+  }, [isMenuActive]);
+
   return (
     <header className="rounded-xl bg-white p-[14px] md:px-[24px] md:py-[16px]">
       <nav className="flex items-center justify-between">
@@ -40,9 +59,17 @@ export const AppBar = () => {
             </AppLink>
           </div>
 
-          <button className="flex items-center justify-center p-[4px] text-appPurple md:p-[8px]">
-            <FaBars className="h-[20px] w-[20px]" />
-          </button>
+          <div className="relative">
+            <button
+              onClick={() => setIsMenuActive(!isMenuActive)}
+              className="flex items-center justify-center p-[4px] text-appPurple md:p-[8px]"
+              id="user-menu-modal"
+            >
+              <FaBars className="pointer-events-none h-[20px] w-[20px]" />
+            </button>
+
+            {isMenuActive && <UserMenuModal />}
+          </div>
         </div>
       </nav>
     </header>
---
+++ src/components/UserMenuModal.tsx
@@ -0,0 +1,11 @@
+const UserMenuModal = () => {
+  return (
+    <div className="absolute bottom-[-40px] right-0 z-[90] flex w-[160px] flex-col rounded-xl bg-white shadow-sm shadow-appPurpleH">
+      <button className="w-full p-[8px] font-medium text-appGreyD">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default UserMenuModal;
