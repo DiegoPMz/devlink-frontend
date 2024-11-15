c949cec3575dc420d943b13349a1dd124443d475
Diego Peralta M <diego.pmz08@gmail.com>
2024-11-14 23:14:31 -0600

    Se creo la utilidad PublishDetailsMap

--- src/components/AppPublishLink.tsx
+++ src/components/AppPublishLink.tsx
@@ -1,21 +1,7 @@
-import { BiLogoDevTo } from "react-icons/bi";
-import {
-  FaFacebook,
-  FaFreeCodeCamp,
-  FaStackOverflow,
-  FaTwitch,
-  FaYoutube,
-} from "react-icons/fa";
-import { FaGitlab, FaHashnode, FaSquareXTwitter } from "react-icons/fa6";
-import { ImLinkedin } from "react-icons/im";
-import { LiaCodepen } from "react-icons/lia";
-import { SiCodewars, SiFrontendmentor } from "react-icons/si";
-import { TbBrandGithubFilled } from "react-icons/tb";
-
-import { FaArrowRight } from "react-icons/fa6";
-
 import { AvailableSocialMedia } from "@/types/social-media";
+import PublishDetailsMap from "@/utilities/PublishDetailsMap";
 import { twclass } from "@/utilities/twclass";
+import { FaArrowRight } from "react-icons/fa6";
 
 interface AppPublishLinkProps {
   size?: "sm";
@@ -23,92 +9,12 @@ interface AppPublishLinkProps {
   link?: string;
 }
 
-interface PublishDetailAttributes {
-  displayName: string;
-  color: string;
-  icon: React.ReactNode;
-}
-
-const publishDetailsMap: Record<AvailableSocialMedia, PublishDetailAttributes> =
-  {
-    codepen: {
-      color: "#FCD900",
-      displayName: "Codepen",
-      icon: <LiaCodepen className="h-[20px] w-[20px]" />,
-    },
-    codewars: {
-      color: "#8A1A50",
-      displayName: "Codewars",
-      icon: <SiCodewars className="h-[20px] w-[20px]" />,
-    },
-    devto: {
-      color: "#333333",
-      displayName: "Dev.to",
-      icon: <BiLogoDevTo className="h-[20px] w-[20px]" />,
-    },
-    facebook: {
-      color: "#2442AC",
-      displayName: "Facebook",
-      icon: <FaFacebook className="h-[20px] w-[20px]" />,
-    },
-    freecodecamp: {
-      color: "#302267",
-      displayName: "FreeCodeCamp",
-      icon: <FaFreeCodeCamp className="h-[20px] w-[20px]" />,
-    },
-    frontendmentor: {
-      color: "#FFFFFF",
-      displayName: "Frontend Mentor",
-      icon: <SiFrontendmentor className="h-[20px] w-[20px]" />,
-    },
-    github: {
-      color: "#1A1A1A",
-      displayName: "GitHub",
-      icon: <TbBrandGithubFilled className="h-[20px] w-[20px]" />,
-    },
-    gitlab: {
-      color: "#EB4925",
-      displayName: "GitLab",
-      icon: <FaGitlab className="h-[20px] w-[20px]" />,
-    },
-    hashnode: {
-      color: "#0330D1",
-      displayName: "Hashnode",
-      icon: <FaHashnode className="h-[20px] w-[20px]" />,
-    },
-    linkedin: {
-      color: "#2D68FF",
-      displayName: "LinkedIn",
-      icon: <ImLinkedin className="h-[20px] w-[20px]" />,
-    },
-    stackoverflow: {
-      color: "#EC7100",
-      displayName: "Stack Overflow",
-      icon: <FaStackOverflow className="h-[20px] w-[20px]" />,
-    },
-    twitch: {
-      color: "#bf94ff",
-      displayName: "Twitch",
-      icon: <FaTwitch className="h-[20px] w-[20px]" />,
-    },
-    twitter: {
-      color: "#43B7E9",
-      displayName: "Twitter",
-      icon: <FaSquareXTwitter className="h-[20px] w-[20px]" />,
-    },
-    youtube: {
-      color: "#EE3939",
-      displayName: "YouTube",
-      icon: <FaYoutube className="h-[20px] w-[20px]" />,
-    },
-  };
-
 export const AppPublishLink = ({
   link,
   socialMedia,
   size,
 }: AppPublishLinkProps) => {
-  const publish = publishDetailsMap[socialMedia];
+  const publish = PublishDetailsMap[socialMedia];
   if (!publish) throw new Error("Invalid parameter social media");
 
   const Container = link ? "a" : "div";
