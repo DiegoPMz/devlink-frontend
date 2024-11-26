55d951f9729d2aec4b476a8e86918dcfaf5c6ee7
Diego Peralta M <diego.pmz08@gmail.com>
2024-11-25 20:06:36 -0600

    Se agrego value a la utilidad PublishDetailsMap

--- src/utilities/PublishDetailsMap.tsx
+++ src/utilities/PublishDetailsMap.tsx
@@ -18,6 +18,7 @@ interface PublishDetailAttributes {
   displayName: string;
   color: string;
   icon: React.ReactNode;
+  value: string;
 }
 
 const PublishDetailsMap: Record<AvailableSocialMedia, PublishDetailAttributes> =
@@ -26,71 +27,85 @@ const PublishDetailsMap: Record<AvailableSocialMedia, PublishDetailAttributes> =
       color: "#FCD900",
       displayName: "Codepen",
       icon: <LiaCodepen className="h-[20px] w-[20px]" />,
+      value: "codepen",
     },
     codewars: {
       color: "#8A1A50",
       displayName: "Codewars",
       icon: <SiCodewars className="h-[20px] w-[20px]" />,
+      value: "codewars",
     },
     devto: {
       color: "#333333",
       displayName: "Dev.to",
       icon: <BiLogoDevTo className="h-[20px] w-[20px]" />,
+      value: "devto",
     },
     facebook: {
       color: "#2442AC",
       displayName: "Facebook",
       icon: <FaFacebook className="h-[20px] w-[20px]" />,
+      value: "facebook",
     },
     freecodecamp: {
       color: "#302267",
       displayName: "FreeCodeCamp",
       icon: <FaFreeCodeCamp className="h-[20px] w-[20px]" />,
+      value: "freecodecamp",
     },
     frontendmentor: {
       color: "#FFFFFF",
       displayName: "Frontend Mentor",
       icon: <SiFrontendmentor className="h-[20px] w-[20px]" />,
+      value: "frontendmentor",
     },
     github: {
       color: "#1A1A1A",
       displayName: "GitHub",
       icon: <TbBrandGithubFilled className="h-[20px] w-[20px]" />,
+      value: "github",
     },
     gitlab: {
       color: "#EB4925",
       displayName: "GitLab",
       icon: <FaGitlab className="h-[20px] w-[20px]" />,
+      value: "gitlab",
     },
     hashnode: {
       color: "#0330D1",
       displayName: "Hashnode",
       icon: <FaHashnode className="h-[20px] w-[20px]" />,
+      value: "hashnode",
     },
     linkedin: {
       color: "#2D68FF",
       displayName: "LinkedIn",
       icon: <ImLinkedin className="h-[20px] w-[20px]" />,
+      value: "linkedin",
     },
     stackoverflow: {
       color: "#EC7100",
       displayName: "Stack Overflow",
       icon: <FaStackOverflow className="h-[20px] w-[20px]" />,
+      value: "stackoverflow",
     },
     twitch: {
       color: "#bf94ff",
       displayName: "Twitch",
       icon: <FaTwitch className="h-[20px] w-[20px]" />,
+      value: "twitch",
     },
     twitter: {
       color: "#43B7E9",
       displayName: "Twitter",
       icon: <FaSquareXTwitter className="h-[20px] w-[20px]" />,
+      value: "twitter",
     },
     youtube: {
       color: "#EE3939",
       displayName: "YouTube",
       icon: <FaYoutube className="h-[20px] w-[20px]" />,
+      value: "youtube",
     },
   };
 
