61c5a2c06364a4b54af00157bf0b40ebba98c4a3
Diego Peralta M <diego.pmz08@gmail.com>
2024-12-05 00:23:45 -0600

    Se creo e implemento la utilidad socialMediaPlaceholders

--- src/pages/LinksPage/components/SocialLinkItem.tsx
+++ src/pages/LinksPage/components/SocialLinkItem.tsx
@@ -3,7 +3,9 @@ import { AppTextField } from "@/components/ui/AppTextField";
 import { useStoreApp } from "@/store";
 import { UserSliceType } from "@/store/userSlice";
 import { ProfileLinks } from "@/types/api-response";
+import { AvailableSocialMedia } from "@/types/social-media";
 import PublishDetailsMap from "@/utilities/PublishDetailsMap";
+import socialMediaPlaceholders from "@/utilities/socialMediaPlaceholders";
 import { HiMiniBars2 } from "react-icons/hi2";
 
 interface SocialLinkItemProps {
@@ -21,6 +23,13 @@ export const SocialLinkItem = ({
 }: SocialLinkItemProps) => {
   const { getErrorLink } = useStoreApp((state) => state.appErrors);
   const options = Object.values(PublishDetailsMap);
+
+  const getPlaceHolder = () => {
+    return (
+      socialMediaPlaceholders[link.platform as AvailableSocialMedia] ??
+      undefined
+    );
+  };
 
   return (
     <div className="flex flex-col gap-[12px] rounded-lg bg-bg-color-secondary px-[20px] py-[24px] xl:px-[24px] xl:py-[28px]">
@@ -56,6 +65,7 @@ export const SocialLinkItem = ({
         <div>
           <label className="text-xs text-txt-color-secondary">Link</label>
           <AppTextField
+            placeholder={getPlaceHolder()}
             onChange={(e) =>
               onChangeLink(link.id, {
                 ...link,
---
+++ src/utilities/socialMediaPlaceholders.ts
@@ -0,0 +1,20 @@
+import { AvailableSocialMedia } from "@/types/social-media";
+
+const socialMediaPlaceholders: Record<AvailableSocialMedia, string> = {
+  codewars: "https://www.codewars.com/users/your-username",
+  codepen: "https://codepen.io/your-username",
+  devto: "https://dev.to/your-username",
+  facebook: "https://www.facebook.com/your.username",
+  freecodecamp: "https://www.freecodecamp.org/your-username",
+  frontendmentor: "https://www.frontendmentor.io/profile/your-username",
+  gitlab: "https://gitlab.com/your-username",
+  github: "https://github.com/your-username",
+  hashnode: "https://hashnode.com/@your-username",
+  linkedin: "https://www.linkedin.com/in/your-username",
+  stackoverflow: "https://stackoverflow.com/users/your-id/your-username",
+  twitter: "https://twitter.com/your_username",
+  twitch: "https://www.twitch.tv/your_username",
+  youtube: "https://www.youtube.com/@your_username",
+};
+
+export default socialMediaPlaceholders;
